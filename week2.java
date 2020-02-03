c01b128decbc7e431b799315392c944aa27794bb
Gustavox98 <60441031+Gustavox98@users.noreply.github.com>
2020-02-03 12:40:24 -0500

    Update week2.java

--- week2.java
+++ week2.java
@@ -1,4 +1,21 @@
-
+/**
+ * 
+ * @author Gustavo 
+ * 2/3/2020
+ *
+ */
 public class week2 {
-
+// Define a variable of type
+	double wallet = 10.35;
+	byte computer = 5;
+	int people = 3;
+// Declare 3 string variables
+	string Name1;
+	string Name2;
+	string Name3;
+// Assign values to those previously created variables
+	Name1 ="Gustavo";
+	Name2 = "Javi";
+	Name3 = "Pedro";
+}
 }
