760f0c9f53f8568e3727afb46035923ca1681244
Gustavox98 <60441031+Gustavox98@users.noreply.github.com>
2020-02-10 12:54:15 -0500

    Update week4.java

--- week4.java
+++ week4.java
@@ -1,5 +1,24 @@
 package week4;
 
 public class week4 {
+	public static void mian(String []args) {
+		Scanner scanner = new Scanner( System.in);
+		double userWeight;
+		double userHeight;
+		double bmi;
+		
+		System.out.println("Please enter your weight");
+		userWeight = scanner.nextdouble();
+		System.out.println("Please enter your height");
+		userHeight = scanner.nextdouble();
+		bmi = (userWeight * 703) /(userHeight * userHeight);
+		if (bmi < 18.5) {
+			System.out.println( "You are uderweight");
+		} else if (bmi<26) {
+			System.out.println("You have optimal weight");
+		} else {
+			
+		}
+	}
 
 }
